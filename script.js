b07596dd885fe7dce2c36a688412fdcc62aebdb5
Alex Druzhinin <aleksey-255@mail.ru>
2024-10-05 00:24:53 +0500

    Lesson19

--- script.js
+++ script.js
@@ -1,210 +1,72 @@
-'use strict';
+const greeting = document.querySelector('.greeting');
+const today = document.querySelector('.today');
+const time = document.querySelector('.time');
+const newYear = document.querySelector('.new-year');
 
-const nameInput = document.getElementById('name');
-const positionSelect = document.getElementById('position');
-const surnameInput = document.getElementById('surname');
-const ageInput = document.getElementById('age');
-const categoryInput = document.getElementById('category');
-const experienceInput = document.getElementById('experience');
-const childrenCheck = document.getElementById('checkbox-1');
-const licensesCheck = document.getElementById('checkbox-2');
-const saveBtn = document.querySelector('.save');
-const tbody = document.querySelector('tbody');
-class Worker {
-  #dataWorker;
-  constructor(name, surName, age, isChildren, dataWorker = []) {
-    this.name = name;
-    this.surName = surName;
-    this.age = age;
-    this.isChildren = isChildren;
-    this.#dataWorker = dataWorker;
-  }
-  get dataWorker() {
-    return this.#dataWorker;
-  }
-  set dataWorker(data) {
-    this.dataWorker.push(data);
-  }
-  init() {
-    saveBtn.addEventListener('click', () => {
-      this.start();
-    });
-    this.displayWorkers();
-  }
-  start() {
-    if (this.checkfields()) {
-      const selectedOptions =
-        positionSelect.options[positionSelect.selectedIndex];
-      if (selectedOptions.value === 'driver') {
-        const driver = this.addDriver();
-        this.dataWorker = driver;
-        this.addWorker(driver);
-        this.clearInputs();
-        console.log(this.#dataWorker);
-      } else if (selectedOptions.value === 'mechanic') {
-        const mechanic = this.addMechanic();
-        this.dataWorker = mechanic;
-        this.addWorker(mechanic);
-        this.clearInputs();
-      }
-    }
-  }
-  displayWorkers() {
-    const storedData = localStorage.getItem('workers');
-    if (storedData) {
-      const workers = JSON.parse(storedData);
-      workers.forEach((worker) => {
-        const row = document.createElement('tr');
-        const del = document.createElement('button');
-        del.textContent = 'Delete';
-        Object.values(worker).forEach((value) => {
-          const cell = document.createElement('td');
-          if (value === true) {
-            cell.textContent = 'Yes';
-          } else if (value === false) {
-            cell.textContent = 'No';
-          } else cell.textContent = value;
-          row.append(cell);
-        });
-        tbody.append(row);
-        row.append(del);
+const daysOfWeek = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+];
 
-        del.addEventListener('click', () => {
-          row.remove();
-        });
-      });
-    }
-  }
-  addWorker(data) {
-    const storedData = localStorage.getItem('workers');
-    if (storedData) {
-      const workers = JSON.parse(storedData);
-      workers.push(data);
-      localStorage.setItem('workers', JSON.stringify(workers));
-    } else {
-      localStorage.setItem('workers', JSON.stringify([data]));
-    }
-    const row = document.createElement('tr');
-    const del = document.createElement('button');
-    del.textContent = 'Delete';
-    Object.values(data).forEach((value) => {
-      const cell = document.createElement('td');
-      if (value === true) {
-        cell.textContent = 'Yes';
-      } else if (value === false) {
-        cell.textContent = 'No';
-      } else cell.textContent = value;
-      row.append(cell);
-    });
-    tbody.append(row);
-    row.append(del);
+const getGreeting = () => {
+  const dateNow = new Date();
+  const dayOfWeek = dateNow.getDate();
+  const dateStop = new Date('1 january 2025').getTime();
+  const newYearRemaining = (dateStop - dateNow) / 1000;
+  let days = Math.floor(newYearRemaining / 60 / 60 / 24);
 
-    const workerToRemove = JSON.parse(JSON.stringify(data));
-    del.addEventListener('click', () => {
-      this.removeWorker(workerToRemove);
-      row.remove();
-    });
+  if (dateNow.getHours() > 6 && dateNow.getHours() < 12) {
+    greeting.textContent = 'Доброе утро';
+  } else if (dateNow.getHours() > 12 && dateNow.getHours() < 18) {
+    greeting.textContent = 'Добрый день';
+  } else if (dateNow.getHours() > 18 && dateNow.getHours() < 24) {
+    greeting.textContent = 'Добрый вечер';
+  } else if (dateNow.getHours() > 24 && dateNow.getHours() < 6) {
+    greeting.textContent = 'Доброй ночи';
   }
-  removeWorker(worker) {
-    const storedData = localStorage.getItem('workers');
-    if (storedData) {
-      const workers = JSON.parse(storedData);
-      const index = workers.findIndex(
-        (item) => JSON.stringify(item) === JSON.stringify(worker)
-      );
-      if (index !== -1) {
-        workers.splice(index, 1);
-        localStorage.setItem('workers', JSON.stringify(workers));
-      }
-    }
-  }
-  addDriver() {
-    const driver = new Driver(
-      nameInput.value,
-      surnameInput.value,
-      ageInput.value,
-      categoryInput.value,
-      experienceInput.value,
-      childrenCheck.checked,
-      licensesCheck.checked
-    );
-    return driver;
-  }
-  addMechanic() {
-    const mechanic = new Mechanic(
-      nameInput.value,
-      surnameInput.value,
-      ageInput.value,
-      categoryInput.value,
-      experienceInput.value,
-      childrenCheck.checked,
-      licensesCheck.checked
-    );
-    return mechanic;
-  }
-  checkfields() {
-    let error = false;
 
-    if (
-      positionSelect.selectedIndex === 0 ||
-      nameInput.value === '' ||
-      surnameInput.value === '' ||
-      ageInput.value === '' ||
-      categoryInput.value === '' ||
-      experienceInput.value === ''
-    ) {
-      error = true;
-    }
-    return !error;
-  }
-  clearInputs() {
-    nameInput.value = '';
-    surnameInput.value = '';
-    ageInput.value = '';
-    categoryInput.value = '';
-    experienceInput.value = '';
-    positionSelect.selectedIndex = 0;
-    childrenCheck.checked = false;
-    licensesCheck.checked = false;
-  }
-}
-
-class Driver extends Worker {
-  constructor(
-    name,
-    surName,
-    age,
-    category,
-    experience,
-    isChildren,
-    isLicenses,
-    position = 'Driver'
+  if (
+    (days === 0 && days >= 5 && days <= 20) ||
+    (days >= 25 && days <= 30) ||
+    (days >= 35 && days <= 40) ||
+    (days >= 45 && days <= 50) ||
+    (days >= 55 && days <= 60) ||
+    (days >= 65 && days <= 70) ||
+    (days >= 75 && days <= 80) ||
+    (days >= 85 && days <= 90)
   ) {
-    super(name, surName, age, isChildren);
-    this.isLicenses = isLicenses;
-    this.position = position;
-    this.category = category;
-    this.experience = experience;
-  }
-}
-class Mechanic extends Worker {
-  constructor(
-    name,
-    surName,
-    age,
-    category,
-    experience,
-    isChildren,
-    isLicenses,
-    position = 'Mechanic'
+    newYear.textContent = `До нового года осталось ${days} дней`;
+  } else if (
+    days === 1 &&
+    days === 21 &&
+    days === 31 &&
+    days === 41 &&
+    days === 51 &&
+    days === 61 &&
+    days === 71 &&
+    days === 81
   ) {
-    super(name, surName, age, isChildren);
-    this.isLicenses = isLicenses;
-    this.position = position;
-    this.category = category;
-    this.experience = experience;
+    newYear.textContent = `До нового года остался ${days} день`;
+  } else if (
+    (days >= 2 && days <= 4) ||
+    (days >= 22 && days <= 24) ||
+    (days >= 32 && days <= 34) ||
+    (days >= 42 && days <= 44) ||
+    (days >= 52 && days <= 54) ||
+    (days >= 62 && days <= 64) ||
+    (days >= 72 && days <= 74) ||
+    (days >= 82 && days <= 84)
+  ) {
+    newYear.textContent = `До нового года осталось ${days} дня`;
   }
-}
+  
+  today.textContent = `Сегодня: ${daysOfWeek[dayOfWeek]}`;
+  time.textContent = `Текущее время: ${dateNow.toLocaleTimeString('en')}`;
+};
 
-const worker = new Worker();
-worker.init();
+setInterval(getGreeting, 1000);
