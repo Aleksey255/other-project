e4e216fd48d1ee7b6a72bd31a1384a24c79865dc
Alex Druzhinin <aleksey-255@mail.ru>
2024-11-18 00:53:25 +0500

    Bonus

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+const filterFilms = document.getElementById('filter-films');
+const cards = document.querySelector('.cards');
+
 const getData = (url) => {
   return fetch(url)
     .then((response) => response.json())
@@ -16,14 +19,99 @@ const sendData = (url, data) => {
     .catch((error) => console.log(error));
 };
 
-getData('db.json').then((data) => {
-  let xhr = new XMLHttpRequest();
+const renderHeroes = (heroes) => {
+  cards.innerHTML = '';
+  heroes.forEach((hero) => {
+    const cardsItem = document.createElement('div');
+    cardsItem.classList.add('cards__item');
 
-  let json = JSON.stringify(data);
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('card__info');
 
-  xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+    const cardName = document.createElement('h2');
+    cardName.classList.add('card__name');
+    cardName.textContent = hero.name;
 
-  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    const cardRealName = document.createElement('p');
+    cardRealName.classList.add('card__real-name');
+    cardRealName.innerHTML = `<b>Реальное имя:</b> ${hero.realName}`;
 
-  xhr.send(json);
+    const cardSpecies = document.createElement('p');
+    cardSpecies.classList.add('card__species');
+    cardSpecies.innerHTML = `<b>Вид:</b> ${hero.species}`;
+
+    const cardCitizenship = document.createElement('p');
+    cardCitizenship.classList.add('card__citizenship');
+    cardCitizenship.innerHTML = `<b>Гражданство:</b> ${hero.citizenship}`;
+
+    const cardGender = document.createElement('p');
+    cardGender.classList.add('card__gender');
+    cardGender.innerHTML = `<b>Пол:</b> ${hero.gender}`;
+
+    const cardStatus = document.createElement('p');
+    cardStatus.classList.add('card__status');
+    cardStatus.innerHTML = `<b>Статус:</b> ${hero.status}`;
+
+    const cardActors = document.createElement('p');
+    cardActors.classList.add('card__actors');
+    cardActors.innerHTML = `<b>Актеры:</b> ${hero.actors}`;
+
+    const cardMovies = document.createElement('p');
+    cardMovies.classList.add('card__movies');
+    cardMovies.innerHTML = `<b>Фильмы:</b> ${hero.movies}`;
+
+    const cardPhoto = document.createElement('img');
+    cardPhoto.classList.add('card__photo');
+    cardPhoto.src = hero.photo;
+
+    cardInfo.append(
+      cardName,
+      cardRealName,
+      cardSpecies,
+      cardCitizenship,
+      cardGender,
+      cardStatus,
+      cardActors,
+      cardMovies
+    );
+    cardsItem.append(cardInfo, cardPhoto);
+
+    cards.append(cardsItem);
+
+    if (hero.realName === undefined) {
+      cardRealName.remove();
+    }
+    if (hero.citizenship === undefined) {
+      cardCitizenship.remove();
+    }
+    if (hero.species === undefined) {
+      cardSpecies.remove();
+    }
+    if (hero.movies === undefined) {
+      cardMovies.remove();
+    }
+  });
+};
+
+getData('dbHeroes.json').then((data) => {
+  renderHeroes(data);
+
+  const filterHeroesByMovie = (movieName) => {
+    const filteredHeroes = data.filter((hero) => {
+      if (movieName === 'all') {
+        return true;
+      }
+      if (Array.isArray(hero.movies)) {
+        return hero.movies.includes(movieName);
+      }
+      return false;
+    });
+    return filteredHeroes;
+  };
+
+  filterFilms.addEventListener('change', (e) => {
+    const selectedMovieName = e.target.value;
+    const filteredHeroes = filterHeroesByMovie(selectedMovieName);
+    renderHeroes(filteredHeroes);
+  });
 });
